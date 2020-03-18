ec965de2ff22b1b41d9a185b04da4ce8c5796b01
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-18 21:15:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -22,7 +22,11 @@ class App < Sinatra::Base
      @result
    end  
    get '/say/:word1/:word2/:word3/:word4/:word5' do 
-     "#{word1}"
+     "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
+   end 
+   
+   get '/:operation/:numer1/:number2' do 
+     
    
      
 end
