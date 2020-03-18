2fbb7f17b5aa3b8ad8da3190d33df1262990852a
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-18 21:05:55 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,11 @@ class App < Sinatra::Base
    end 
    
    get '/square/:number' do 
-     @result = params[:number].to_i * params[:number].to_i
+     @result = params[:number].to_i*params[:number].to_i
      "#{@result}"
    end 
+   
+   get '/say/:number/:phrase' do 
+     
      
 end
