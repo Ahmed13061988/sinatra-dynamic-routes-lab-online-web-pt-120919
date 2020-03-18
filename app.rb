a23f42cf1ab3de6e5766c1fa45d7b4735822d972
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-18 21:18:35 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -17,7 +17,7 @@ class App < Sinatra::Base
      @number = params[:number].to_i
      @phrase = params[:phrase]
      @number.times do 
-       @result =+ "#{phrase}/n"
+       @result += "#{phrase}/n"
      end 
      @result
    end  
